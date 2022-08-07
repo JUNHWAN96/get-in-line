131f76ef9b746842262b1ebf16f27f0a8e9ae023
junhwan <mynameisljh@naver.com>
2022-08-07 16:56:33 +0900

    gitflow-feature-stash: 프로젝트세팅

---
+++ src/main/java/com/jh/getinLine/GetinLineApplication.java
@@ -0,0 +1,13 @@
+package com.jh.getinLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetinLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetinLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jh/getinLine/GetinLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.getinLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetinLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
